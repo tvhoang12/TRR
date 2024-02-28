b00f4cc189415b2c281a38026498ed6bf58e7370
tvhoang12 <HoangTV.B22AT129@stu.ptit.edu.vn>
2024-02-28 21:09:11 +0700

    Bt tren slide

---
+++ DFS.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<vector<int>> adj(n + 1);
+    stack<int> st;
+    bool vis[n+1];
+    memset(vis, 0, sizeof(vis));
+    cin.ignore();
+    
+    for(int i = 1; i <= n; i++) {
+        string str;
+        getline(cin, str);
+        stringstream ss(str);
+        int x;  
+        vector<int> temp;
+
+        while(ss >> x) {
+            temp.push_back(x);
+        }
+
+        adj[i].insert(adj[i].end(),temp.begin(),temp.end());
+    }
+
+    st.push(1);
+    vis[1] = 1;
+    cout << 1 << " ";
+
+    while (st.size())
+        {
+            int u = st.top();
+            st.pop();
+
+            for (int i = 0; i < adj[u].size(); i++)
+            {
+                int v = adj[u][i];
+                if(!vis[v])
+                {
+                    cout << v << " ";
+                    vis[v] = 1;
+                    st.push(u);
+                    st.push(v);
+                    break;
+                }
+            }
+        }
+        cout << endl;
+}
